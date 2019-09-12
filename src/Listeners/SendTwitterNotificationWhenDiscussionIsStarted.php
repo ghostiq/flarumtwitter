9d8094bb6e2f7da47db704a02978dae0e196269a
Czarodziej Nocy <6280789+ghostiq@users.noreply.github.com>
2019-09-12 17:25:13 +0200

    Update SendTwitterNotificationWhenDiscussionIsStarted.php

--- src/Listeners/SendTwitterNotificationWhenDiscussionIsStarted.php
+++ src/Listeners/SendTwitterNotificationWhenDiscussionIsStarted.php
@@ -7,21 +7,23 @@ use Flarum\Tags\Tag;
 use Illuminate\Support\Collection;
 
 use Flarum\Settings\SettingsRepositoryInterface;
+use Flarum\Http\UrlGenerator;
 use DG\Twitter\Twitter;
 
 class SendTwitterNotificationWhenDiscussionIsStarted
 {
-
+    protected $baseDiscussionUrl;
     protected $twitter;
     protected $tagIdToFollow;
     protected $tagIdToFollow1;    
 
-    public function __construct(SettingsRepositoryInterface $settings)
+    public function __construct(SettingsRepositoryInterface $settings, UrlGenerator $urlGenerator)
     {
         $consumerAPI = $settings->get('ghostiq-flarumtwitter.consumerAPI');
         $consumerAPISecret = $settings->get('ghostiq-flarumtwitter.consumerAPISecret');
         $accessToken = $settings->get('ghostiq-flarumtwitter.accessToken');
         $accessTokenSecret = $settings->get('ghostiq-flarumtwitter.accessTokenSecret');
+        $this->baseDiscussionUrl = $urlGenerator->to('forum')->base() . '/d/';
         $this->tagIdToFollow = $settings->get('ghostiq-flarumtwitter.tagIdToFollow');
         $this->tagIdToFollow1 = $settings->get('ghostiq-flarumtwitter.tagIdToFollow1');
         if (!$consumerAPI || !$consumerAPISecret || !$accessToken || !$accessTokenSecret || !$this->tagIdToFollow) {
@@ -51,9 +53,9 @@ class SendTwitterNotificationWhenDiscussionIsStarted
         }
       
         try {
-        	$debug = var_export($discussion, true);
-            mail('ghostiq@gmail.com', 'Debug', $discussion);
-            $tweet = $this->twitter->send($discussion->title . ' '); // you can add $imagePath or array of image paths as second argument            
+        	//$debug = var_export($this->tagIdToFollow . "\r\n" . $this->tagIdToFollow1 . "\r\n" . $this->urlGenerator->to("forum"), true);
+            //mail('ghostiq@gmail.com', 'Debug', $debug);
+            $tweet = $this->twitter->send($discussion->title . ' ' . $this->baseDiscussionUrl . $discussion->id . '-' . $discussion->slug); // you can add $imagePath or array of image paths as second argument            
         }
         catch (DG\Twitter\TwitterException $e)
         {
